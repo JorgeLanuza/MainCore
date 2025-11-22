5f9e2d0f7c0aca5fdb4453187336bda44b3aaebb
jorgeperezdev <jorgeplanuza@gmail.com>
2025-11-22 23:08:17 +0100

    Feat: pnpm setup

---
+++ maincore.client/src/app/providers/styles/prime-ng.provider.ts
@@ -0,0 +1,31 @@
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { FileUploadModule } from 'primeng/fileupload';
+import { CardModule } from 'primeng/card';
+import { CheckboxModule } from 'primeng/checkbox';
+import { TooltipModule } from 'primeng/tooltip';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ProgressBarModule } from 'primeng/progressbar';
+
+export const primeNgProviders = [
+  ButtonModule,
+  InputTextModule,
+  TableModule,
+  DialogModule,
+  FileUploadModule,
+  CardModule,
+  CheckboxModule,
+  TooltipModule,
+  ToastModule,
+  ConfirmDialogModule,
+  ProgressSpinnerModule,
+  RadioButtonModule,
+  ToolbarModule,
+  ProgressBarModule,
+];
